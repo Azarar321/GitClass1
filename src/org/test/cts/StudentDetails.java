c3a1d6c531027d527ad0ffaadfac4a3670ee8ba3
Azarar321 <azarar321@gmail.com>
2021-02-06 15:06:58 +0530

    New Methods Added

--- src/org/test/cts/StudentDetails.java
+++ src/org/test/cts/StudentDetails.java
@@ -3,7 +3,17 @@ package org.test.cts;
 public class StudentDetails {
 	
 	private void studentName() {
-		
+	
+
+	}
+	
+	private void studentID() {
+	
+
+	}
+	
+	private void studentAddress() {
+	
 
 	}
 	
